08c43b647e325d0baa43417ad0bd00b66831aca7
Anna_Matveeva <75207035+AnikaBaykus@users.noreply.github.com>
2021-08-28 15:16:23 +0300

    Updates

--- static/js/main.81dbdc59.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-03-image-finder2"]=this["webpackJsonpgoit-react-hw-03-image-finder2"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n.n(c),r=n(3),a=n.n(r),o=(n(8),n(9),n(0));var s=function(){return Object(o.jsx)("div",{className:"App"})};a.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.81dbdc59.chunk.js.map
