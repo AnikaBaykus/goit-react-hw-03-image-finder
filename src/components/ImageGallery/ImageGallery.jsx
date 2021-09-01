18c75966639caef238d79e93707d3d6c887db21e
Anna_Matveeva <75207035+AnikaBaykus@users.noreply.github.com>
2021-09-01 21:11:52 +0300

    Исправляет ключи для изображений

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,14 +1,13 @@
 import PropTypes from 'prop-types';
 import ImageGalleryItem from '../ImageGalleryItem';
 import s from './ImageGallery.module.css';
-
 function ImageGallery({ hits, onClickImage }) {
   // console.log(hits);
   return (
     <ul className={s.ImageGallery}>
-      {hits.map(({ webformatURL, tags, largeImageURL }, i) => (
+      {hits.map(({ id, webformatURL, tags, largeImageURL }) => (
         <ImageGalleryItem
-          key={i}
+          key={id}
           webformatURL={webformatURL}
           tags={tags}
           onClickImage={onClickImage}
@@ -24,6 +23,7 @@ ImageGallery.propTypes = {
       webformatURL: PropTypes.string.isRequired,
       tags: PropTypes.string.isRequired,
       largeImageURL: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
     }),
   ),
   onClickImage: PropTypes.func,
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,11 +1,8 @@
 import PropTypes from 'prop-types';
 import s from './ImageGalleryItem.module.css';
-import { v4 as uuidv4 } from 'uuid';
-
 function ImageGalleryItem({ webformatURL, tags, largeImageURL, onClickImage }) {
-  const imgId = uuidv4();
   return (
-    <li key={imgId} className={s.ImageGalleryItem} onClick={onClickImage}>
+    <li className={s.ImageGalleryItem} onClick={onClickImage}>
       <img
         src={webformatURL}
         alt={tags}
